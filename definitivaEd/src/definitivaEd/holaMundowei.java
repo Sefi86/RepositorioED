b756b0dbe0c33670d35f7adbdd1c1740e55195bf
Sefi86 <https://github.com/Sefi86/TrabajoED.git>
2015-04-15 12:11:41 +0200

    Segundo intento

--- definitivaEd/src/definitivaEd/holaMundowei.java
+++ definitivaEd/src/definitivaEd/holaMundowei.java
@@ -5,7 +5,8 @@ public class holaMundowei {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo weiii");
-
+		System.out.println("!!!!");
+		System.out.println("Que tal?");
 	}
 
 }
