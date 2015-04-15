1cdf247020cd2257fe6a3233515b41815a6aa6e1
Sefi86 <pepiadalid86@gmail.com>
2015-04-15 11:21:48 +0200

    Pimer intento

---
+++ definitivaEd/src/definitivaEd/holaMundowei.java
@@ -0,0 +1,11 @@
+package definitivaEd;
+
+public class holaMundowei {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo weiii");
+
+	}
+
+}
